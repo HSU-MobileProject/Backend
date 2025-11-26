8b42d7f033cef12b8bead51252e438df7d3e1ced
mint0326 <jmj200436@hansung.ac.kr>
2025-11-26 18:44:07 +0900

    fix: index.js 충돌 해결

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,48 @@
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-// 각 모듈 불러오기
-const authLogic = require('./src/auth');
-const notiLogic = require('./src/notifications');
-const payLogic = require('./src/payments');
-const projectCRUD = require('./src/projects/crud');
-const projectFavorites = require('./src/projects/favorite');
-const projectSearch = require('./src/projects/search');
+// 각 모듈 파일 불러오기 (중복 제거 및 경로 통일)
 const authLogic = require("./src/auth");
 const notiLogic = require("./src/notifications");
 const payLogic = require("./src/payments");
 const likeLogic = require("./src/likes");
 const chatLogic = require("./src/chat");
-const projectLogic = require("./src/projects");
+const projectLogic = require("./src/projects"); // 검색 키워드 생성 트리거
+
+const projectCRUD = require("./src/projects/crud");
+const projectFavorites = require("./src/projects/favorite");
+const projectSearch = require("./src/projects/search");
 
 // 함수 내보내기 (Cloud Functions에 배포될 이름)
+// Auth
 exports.createProfile = authLogic.createProfile;
 exports.deleteProfile = authLogic.deleteProfile;
-exports.sendChatNotification = notiLogic.sendChatNotification;
-exports.verifyPayment = payLogic.verifyPayment;
 
-// 프로젝트 CRUD
+// Notification & Chat
+exports.sendChatNotification = notiLogic.sendChatNotification;
+exports.updateChatRoomLastMessage = chatLogic.updateChatRoomLastMessage;
+exports.onPaymentCreate = notiLogic.onPaymentCreate;
+
+// Project & Likes
+exports.generateKeywords = projectLogic.generateKeywords; // 검색 키워드 생성
+exports.onLikeChange = likeLogic.onLikeChange; // 좋아요 카운트 동기화
+
+//API 요청
+// Payment
+exports.createPayment = payLogic.createPayment;
+
+// Project CRUD
 exports.createProject = projectCRUD.createProject;
 exports.listProjects = projectCRUD.listProjects;
 exports.getProject = projectCRUD.getProject;
 exports.updateProject = projectCRUD.updateProject;
 exports.deleteProject = projectCRUD.deleteProject;
 
-// 즐겨찾기
+// Favorites
 exports.addFavorite = projectFavorites.addFavorite;
 exports.removeFavorite = projectFavorites.removeFavorite;
 exports.listFavorites = projectFavorites.listFavorites;
 
-// 검색/필터
+// Search
 exports.searchProjects = projectSearch.searchProjects;
 exports.filterByCategory = projectSearch.filterByCategory;
-exports.onPaymentCreate = notiLogic.onPaymentCreate;
-exports.createPayment = payLogic.createPayment;
-exports.onLikeChange = likeLogic.onLikeChange;
-exports.updateChatRoomLastMessage = chatLogic.updateChatRoomLastMessage;
-exports.generateKeywords = projectLogic.generateKeywords;
